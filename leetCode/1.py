196f0610eeeda9c36dfb5c575825033e0ac5780c
Zaveshaa <127344512+zaveshaa@users.noreply.github.com>
2025-05-08 23:12:29 +0300

    leetcode first pass

--- leetCode/1.py
+++ leetCode/1.py
@@ -1,11 +1,22 @@
-# вводится массив чисел, и сумма каких то двух чисел даст таргет число
-
+# pass
 numbers = list(map(int, input().split()))
 target = int(input())
 
 sums = []
 
-for i in range(len(numbers)):
-    for j in range(i + 1, len(numbers)):
-        sums.append(numbers[i] + numbers[j])
-# теперь осталось сдлеать проверку индекса подходящих чисел
+for i in range(len(numbers)):  # впринципе по всем
+    for j in range(i + 1, len(numbers)):  # по повторениям
+        sums.append(numbers[i] + numbers[j])  # в массив сумм добавляем все возможные
+        if numbers[i] + numbers[j] == target:  # сравниваем суммы с таргетом
+            print([i, j])
+
+
+'''from typing import List
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+        return []''' # это для лита, но время выполнения конечно паршивое, щас будем исправлять
